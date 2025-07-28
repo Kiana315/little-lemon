2455bc00aa9e1889e3365fac15958a6cd9f50bbb
Kiana315 <93419255+Kiana315@users.noreply.github.com>
2025-07-27 22:12:41 -0600

    Add Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,25 @@
+import './Hero.css';
+import { Link } from 'react-router-dom';
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-text">
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>
+          We are a family owned Mediterranean restaurant, 
+          focused on traditional recipes served with a modern twist.
+        </p>
+        <Link to="/booking">
+          <button className='button-primary'>Reserve a Table</button>
+        </Link>
+      </div>
+      <div className="hero-image">
+        <img src="/images/hero-image.jpg" alt="Delicious Mediterranean dish" />
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
