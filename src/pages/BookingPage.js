ef8ae8f0718e01c07ff16d2c0782070b2ed4159d
Kiana315 <93419255+Kiana315@users.noreply.github.com>
2025-07-27 22:11:58 -0600

    Create BookingPage.js

---
+++ src/pages/BookingPage.js
@@ -0,0 +1,11 @@
+// src/pages/BookingPage.js
+function BookingPage() {
+  return (
+    <section>
+      <h1>Book a Table</h1>
+      <p>Reservation form coming soon...</p>
+    </section>
+  );
+}
+
+export default BookingPage;
