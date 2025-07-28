8c54c61d90d9028418f91f641c30790781c849ab
Kiana315 <93419255+Kiana315@users.noreply.github.com>
2025-07-27 22:11:44 -0600

    Add Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,40 @@
+import "./Footer.css";
+
+
 function Footer() {
   return (
     <footer>
-        <div className="footer-logo">
-            <img src="/images/logo.png" alt="Little Lemon logo" />
+        <div className="footer-container">
+            <div className="footer-logo">
+                <img src="/images/logo-lg.png" alt="Little Lemon logo" />
+            </div>
+            <div className="footer-links">
+                <div className="footer-column">
+                    <h4>Doormat Navigation</h4>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                    ...
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <h4>Contact</h4>
+                    <ul>
+                        <li>Address</li>
+                        <li>Phone number</li>
+                        <li>Email</li>
+                    </ul>
+                </div>
+                <div className="footer-column">
+                    <h4>Social Media Links</h4>
+                    <ul>
+                        <li>Facebook</li>
+                        <li>Instagram</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div className="footer-nav">
-            <h4>Doormat Navigation</h4>
-            <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            ...
-            </ul>
-        </div>
-        <div className="footer-contact">
-            <h4>Contact</h4>
-            <p>Address</p>
-            <p>Phone</p>
-            <p>Email</p>
-        </div>
-        <div className="footer-social">
-            <h4>Social Media Links</h4>
-            <p>Facebook, Instagram, etc.</p>
-        </div>
-        </footer>
+    </footer>
 
   );
 }
