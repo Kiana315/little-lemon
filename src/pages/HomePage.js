963cb31bf06208f78c70aea197db2017b08584ad
Kiana315 <93419255+Kiana315@users.noreply.github.com>
2025-07-27 22:11:56 -0600

    Create HomePage.js

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+// src/pages/HomePage.js
+import Hero from '../components/Hero';
+import Specials from '../components/Specials';
+import About from '../components/About';
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Specials />
+      <About />
+    </>
+  );
+}
+
+export default HomePage;
