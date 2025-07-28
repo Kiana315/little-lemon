2ebc47960a9674389e9aec2e91510624d1476dc6
Kiana315 <93419255+Kiana315@users.noreply.github.com>
2025-07-27 22:12:47 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,8 @@
 function Header() {
-  return (
-    <header>
-      {/* logo 和标题等 */}
-      <img src="/images/Asset 20@4x.png" alt="Little Lemon logo" />
-
-    </header>
-  );
+    return (
+        <div className="logo-container">
+            <img src="/images/logo-md.png" alt="Little Lemon logo" />
+        </div>
+    );
 }
 export default Header;
